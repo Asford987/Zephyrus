5e375ebcf296c3b212b83a4b3329fd920f1a519c
Asford987 <fabricio.asfora.001@gmail.com>
2025-05-01 17:21:46 -0300

    denugged last handler file

--- lib/Passes/handlers/SoftmaxHandler.cpp
+++ lib/Passes/handlers/SoftmaxHandler.cpp
@@ -22,37 +22,42 @@ namespace zephyrus{
       auto inputType = RankedTensorType::get(inputShape, f32);
       int axis = inputShape.size() - 1;
   
-      // 1. reduce_max(x)
       std::vector<int64_t> reducedShape = inputShape;
       reducedShape[axis] = 1;
       auto reducedType = RankedTensorType::get(reducedShape, f32);
+      auto shapeType  = RankedTensorType::get(
+          {static_cast<int64_t>(reducedShape.size())}, builder.getI64Type());
+      DenseElementsAttr shapeAttr =
+          DenseElementsAttr::get(shapeType,
+                                 llvm::ArrayRef<int64_t>(reducedShape));   // ArrayRef, no makeArrayRef
   
       Value maxVals = builder.create<tosa::ReduceMaxOp>(
           loc, reducedType, lastOutput, builder.getI64IntegerAttr(axis));
   
-      // 2. reshape max to broadcast shape
+      Value shapeTensor =
+          builder.create<tosa::ConstOp>(loc, shapeType, shapeAttr);
       Value maxValsBroadcast = builder.create<tosa::ReshapeOp>(
-          loc, inputType, maxVals, builder.getI64ArrayAttr(reducedShape));
+          loc, inputType, maxVals, shapeTensor);
   
-      // 3. subtract max: x - max(x)
       Value centered = builder.create<tosa::SubOp>(loc, inputType, lastOutput, maxValsBroadcast);
   
-      // 4. exp(x - max)
       Value expTensor = builder.create<tosa::ExpOp>(loc, inputType, centered);
   
-      // 5. reduce_sum(exp(...))
       Value sumExp = builder.create<tosa::ReduceSumOp>(
           loc, reducedType, expTensor, builder.getI64IntegerAttr(axis));
   
-      // 6. broadcast sum back
       Value sumExpBroadcast = builder.create<tosa::ReshapeOp>(
-          loc, inputType, sumExp, builder.getI64ArrayAttr(reducedShape));
-  
-      // 7. divide exp / sum
+          loc, inputType, sumExp, shapeTensor);
+      
+      auto shiftType  = RankedTensorType::get({}, builder.getIntegerType(32));
+      auto zeroShift  = builder.create<tosa::ConstOp>(
+          loc, shiftType,
+          DenseElementsAttr::get(shiftType, 0));
+      
       Value reciprocal = builder.create<tosa::ReciprocalOp>(loc, inputType, sumExpBroadcast);
-      Value softmaxOut = builder.create<tosa::MulOp>(loc, inputType, expTensor, reciprocal);
-  
-      // Final output
+      Value softmaxOut = builder.create<tosa::MulOp>(
+          loc, inputType, expTensor, reciprocal, zeroShift);
+
       lastOutput = softmaxOut;
   }
 
